2a72cbbe0bd858da551258dd76223c4ad22f450d
Steve Anderson <steve_anderson@Steves-Mac-mini.local>
2026-04-17 18:08:12 -0600

    fix(clerk): Simplify middleware matcher to catch all routes

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -20,7 +20,6 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
